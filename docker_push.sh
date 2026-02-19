af64772b0dd31a14aa9cff65fc159449a45ca2e5
AlexAzalor <xy55xy.azalor@gmail.com>
2026-02-19 07:59:54 +0000

    v1.1.0

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+
 # Push to docker hub
 # Add docker login?
 # Variables?
+
+echo "Do you check Quick Movies? (yes/no):"
+read -r answer
+
+if [[ "$answer" != "yes" && "$answer" != "y" ]]; then
+    echo "Script stopped. Please check Quick Movies first."
+    exit 1
+fi
+
+echo "Proceeding with Docker build and push..."
+
 poetry run ruff check .
 poetry run mypy
 poetry run ruff format .
